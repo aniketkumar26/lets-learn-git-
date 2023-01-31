e5f57aa88371ac5b039189595dd4caa7c84c270c
Aniket Tiwari <kumar.anikumar89@gmail.com>
2023-01-31 14:32:50 +0530

    kumar.cpp

--- hello.cpp
+++ hello.cpp
@@ -12,7 +12,7 @@ int main() {
   sum = first_number + second_number;
 
   // prints sum 
-  cout << first_number << " + " <<  second_number << " = " << sum;     
+  cout << first_numberisthere << " + " <<  second_number << " = " << sum;     
 
   return 0;
 }
